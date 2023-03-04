19e4e944b1b9a484a59125bc48f5da604550dc92
JonathanLara <jonathan.lara@footloose.pe>
2023-03-04 11:53:24 -0500

    [UPDATE] ChessGameBoard code smell reducir la complejidad --> initializaBoard()

--- src/GUI/ChessGameBoard.java
+++ src/GUI/ChessGameBoard.java
@@ -164,56 +164,46 @@ public class ChessGameBoard extends JPanel{
      * pieces added.
      */
     public void initializeBoard(){
-        resetBoard( false );
-        for ( int i = 0; i < chessCells.length; i++ ){
-            for ( int j = 0; j < chessCells[0].length; j++ ){
-                ChessGamePiece pieceToAdd;
-                if ( i == 1 ) // black pawns
-                {
-                    pieceToAdd = new Pawn( this, i, j, ChessGamePiece.BLACK );
-                }
-                else if ( i == 6 ) // white pawns
-                {
-                    pieceToAdd = new Pawn( this, i, j, ChessGamePiece.WHITE );
-                }
-                else if ( i == 0 || i == 7 ) // main rows
-                {
-                    int colNum =
-                        i == 0 ? ChessGamePiece.BLACK : ChessGamePiece.WHITE;
-                    if ( j == 0 || j == 7 ){
-                        pieceToAdd = new Rook( this, i, j, colNum );
-                    }
-                    else if ( j == 1 || j == 6 ){
-                        pieceToAdd = new Knight( this, i, j, colNum );
-                    }
-                    else if ( j == 2 || j == 5 ){
-                        pieceToAdd = new Bishop( this, i, j, colNum );
-                    }
-                    else if ( j == 3 ){
-                        pieceToAdd = new King( this, i, j, colNum );
-                    }
-                    else
-                    {
-                        pieceToAdd = new Queen( this, i, j, colNum );
-                    }
-                }
-                else
-                {
-                    pieceToAdd = null;
-                }
-                chessCells[i][j] = new BoardSquare( i, j, pieceToAdd );
-                if ( ( i + j ) % 2 == 0 ){
-                    chessCells[i][j].setBackground( Color.WHITE );
-                }
-                else
-                {
-                    chessCells[i][j].setBackground( Color.BLACK );
-                }
-                chessCells[i][j].addMouseListener( listener );
-                this.add( chessCells[i][j] );
+        resetBoard(false);
+        for (int i = 0; i < chessCells.length; i++) {
+            for (int j = 0; j < chessCells[0].length; j++) {
+                createBoardSquare(i, j);
             }
         }
     }
+    private void createBoardSquare(int i, int j) {
+    ChessGamePiece pieceToAdd = null;
+    Color backgroundColor = (i + j) % 2 == 0 ? Color.WHITE : Color.BLACK;
+    int colNum = i == 0 ? ChessGamePiece.BLACK : ChessGamePiece.WHITE;
+    if (i == 1 || i == 6) { // black pawns or white pawns
+        pieceToAdd = new Pawn(this, i, j, i == 1 ? ChessGamePiece.BLACK : ChessGamePiece.WHITE);
+    } else if (i == 0 || i == 7) { // main rows
+        switch (j) {
+            case 0:
+            case 7:
+                pieceToAdd = new Rook(this, i, j, colNum);
+                break;
+            case 1:
+            case 6:
+                pieceToAdd = new Knight(this, i, j, colNum);
+                break;
+            case 2:
+            case 5:
+                pieceToAdd = new Bishop(this, i, j, colNum);
+                break;
+            case 3:
+                pieceToAdd = new King(this, i, j, colNum);
+                break;
+            case 4:
+                pieceToAdd = new Queen(this, i, j, colNum);
+                break;
+        }
+    }
+    chessCells[i][j] = new BoardSquare(i, j, pieceToAdd);
+    chessCells[i][j].setBackground(backgroundColor);
+    chessCells[i][j].addMouseListener(listener);
+    this.add(chessCells[i][j]);
+}
     // ----------------------------------------------------------
     /**
      * Clears the colors on the board.
